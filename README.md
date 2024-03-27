d025da2af6a5aa9f13b1d121944c49b8c27613ca
Thonysilva <thony.libresolucoes@gmail.com>
2024-03-26 22:44:56 -0300

    Front-End Login

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # login-basico-ReactNative
 Um login Front End Básico em React Native Expo
+
+# Pré-requisitos
+    Node.js
+    Expo CLI
+
+# Instalação
+    Clone este repositório.
+    Navegue até o diretório do projeto no terminal.
+    Execute npm install para instalar as dependências.
+    Execute npm start para iniciar o aplicativo.
+
+# Uso
+    Após iniciar o aplicativo com npm start, um servidor Expo será iniciado e você poderá abrir o aplicativo em seu dispositivo físico usando o aplicativo Expo Go ou em um emulador iOS/Android.
+    Siga as instruções na tela para navegar pelo aplicativo.
---
+++ src/LoginScreen.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    alert('Logou confia');
+  };
+
+  const handleRegister = () => {
+    alert('foi para a tela de registrar xD');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <Text style={styles.registerText} onPress={handleRegister}>NÃ£o tem Login? Registre-se</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  loginButton: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#007bff',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  registerText: {
+    fontSize: 14,
+    color: '#007bff',
+  },
+});
+
+export default LoginScreen;
